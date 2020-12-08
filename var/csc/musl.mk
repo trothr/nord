a9e15df2e2abbadc04603a0f3118f0762c688624
Rick Troth <trothr@gmail.com>
2020-12-07 22:48:11 -0500

    tweaks in following Physix prep

--- var/csc/musl.mk
+++ var/csc/musl.mk
@@ -7,23 +7,26 @@
 
 SC_APN		=	musl
 SC_APV		=	1.2.1
-SC_ARC		=	tar.gz
 SC_VRM		=	$(SC_APN)-$(SC_APV)
+SC_ARC		=	tar.gz
 
 SC_URL		=	\
-		http://www.musl-libc.org/releases/$(SC_VRM).$(SC_ARC) \
-		http://www.musl-libc.org/releases/$(SC_VRM).$(SC_ARC).asc
+	       http://www.musl-libc.org/releases/$(SC_VRM).$(SC_ARC) \
+	       http://www.musl-libc.org/releases/$(SC_VRM).$(SC_ARC).asc
 
 #SC_SOURCE_VERIFY = gpg --verify arc/$(SC_APN)/$(SC_VRM).$(SC_ARC).asc
 #gpg --keyserver hkp://pool.sks-keyservers.net/ --recv-keys 0x56bcdb593020450f
+#gpg --keyid-format long --verify arc/$(SC_APN)/$(SC_VRM).$(SC_ARC).asc
 
 #SC_FETCH	=	
-#SC_SOURCE	=	
+#SC_CONFIG	=	./configure --prefix=/usr --disable-nls
+#SC_CONFIG	=	./configure --disable-nls
+#SC_CONFIG	=	./configure --disable-nls \
+#				--without-selinux --enable-man \
+#				--sysconfdir=/etc
 SC_CONFIG	=	./configure --prefix=/usr \
 					--enable-static --disable-shared
-
-#SC_BUILD	=	
-#SC_FIXUP	=	strip ...
 #SC_INSTALL	=	
+#SC_FIXUP	=	
 
 
--- var/csc/pth.mk
+++ var/csc/pth.mk
@@ -10,8 +10,8 @@ SC_APV		=	2.0.7
 SC_VRM		=	$(SC_APN)-$(SC_APV)
 SC_ARC		=	tar.gz
 
-SC_URL	=	 ftp://ftp.gnu.org/gnu/$(SC_VRM)/$(SC_VRM).$(SC_ARC) \
-		 ftp://ftp.gnu.org/gnu/$(SC_VRM)/$(SC_VRM).$(SC_ARC).sig
+SC_URL	=	 ftp://ftp.gnu.org/gnu/$(SC_APN)/$(SC_VRM).$(SC_ARC) \
+		 ftp://ftp.gnu.org/gnu/$(SC_APN)/$(SC_VRM).$(SC_ARC).sig
 
 SC_SOURCE_VERIFY = gpg --verify arc/$(SC_APN)/$(SC_VRM).$(SC_ARC).sig
 #gpg --keyserver hkp://pool.sks-keyservers.net/ --recv-keys 0x6e744acba9c09e30
